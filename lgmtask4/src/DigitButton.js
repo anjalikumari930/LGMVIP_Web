e2e4b0a976457c189e0ed675b898becf8fce5dab
anjalikumari930 <anjalikumari93045@gmail.com>
2023-05-13 16:27:10 +0530

    source code

--- lgmtask4/src/DigitButton.js
+++ lgmtask4/src/DigitButton.js
@@ -1,9 +1,11 @@
-import { ACTIONS } from "./App";
-export default function DigitButton({dispatch,digit}){
-return ( <button
-    onClick={() => diapatch({type: ACTIONS.ADD_DIGIT,payload:{digit}})}
+import { ACTIONS } from "./App"
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
     >
-    {digit}
+      {digit}
     </button>
-)
+  )
 }
